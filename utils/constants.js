eefc26513c7e4aa96616d055b67738f8c387c110
vahidkk <kohandel.vahid@gmail.com>
2022-03-08 20:14:34 +0330

    home-page(services,hero,contact,newsletter) added

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,5 @@
+import { Repeat, HelpCircle, Shield } from "react-feather";
+
 export const links = [
   {
     id: 1,
@@ -13,5 +15,26 @@ export const links = [
     id: 3,
     text: "products",
     url: "/products",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: <Shield />,
+    title: "Security",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+  {
+    id: 2,
+    icon: <HelpCircle />,
+    title: "Support",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+  {
+    id: 3,
+    icon: <Repeat />,
+    title: "Refund",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
   },
 ];
